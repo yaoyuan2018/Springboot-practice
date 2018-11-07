3d1c31eb983d8230cf9fdf33e1d133cb37dc2d6f
yy <yy494818027@163.com>
2018-11-07 10:30:57 +0800

    自己写组合注解

---
+++ src/main/java/ch3/annotation/DemoConfig.java
@@ -0,0 +1,6 @@
+package ch3.annotation;
+
+@YYConfiguration("ch3.annotation")
+public class DemoConfig {
+}
+//使用@YYConfiguration组合注解替代@Configuration和@ComponentScan
---
+++ src/main/java/ch3/annotation/YYConfiguration.java
@@ -0,0 +1,15 @@
+package ch3.annotation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Configuration
+@ComponentScan
+public @interface YYConfiguration {
+    String[] value() default{};
+}
